02b49ec2b4b3709fb8887e5515017b152af1018d
“yahyaroot” <amriyahya.email@gmail.com>
2024-06-19 21:15:40 +0800

    filter dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,16 +3,32 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Transaction;
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Illuminate\Support\Carbon;
 use Filament\Widgets\StatsOverviewWidget\Card;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+
     protected function getStats(): array
     {
-      $pemasukan= Transaction::incomes()->get()->sum('amount');
 
-      $pengeluaran= Transaction::expenses()->get()->sum('amount');
+
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+            
+
+      $pemasukan= Transaction::incomes()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
+
+      $pengeluaran= Transaction::expenses()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
 
         return [
             Card::make('Pemasukan', $pemasukan)
